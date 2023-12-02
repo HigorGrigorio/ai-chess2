7d96522785a7da67e19b85826876b93cc99c72ad
Higor Grigorio <higorgrigorio@gmail.com>
2023-12-01 23:46:43 -0300

    feat: king moves.

--- engine.py
+++ engine.py
@@ -147,7 +147,16 @@ class GameState:
         self._get_bishop_moves(r, c, moves)
 
     def _get_king_moves(self, r, c, moves):
-        pass
+        king_moves = ((-1, -1), (-1, 0), (-1, 1), (0, -1),
+                      (0, 1), (1, -1), (1, 0), (1, 1))
+        ally_color = 'w' if self.white_to_move else 'b'
+        for i in range(8):
+            end_row = r + king_moves[i][0]
+            end_col = c + king_moves[i][1]
+            if 0 <= end_row < 8 and 0 <= end_col < 8:  # on board
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != ally_color:  # not an ally piece (empty or enemy piece)
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
 
 
 class Move:
