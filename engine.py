f771e0100f1b1a41a139d96fa683da2a0f1eea9c
Higor Grigorio <higorgrigorio@gmail.com>
2023-12-01 23:45:09 -0300

    feat: bishop moves.

--- engine.py
+++ engine.py
@@ -124,7 +124,25 @@ class GameState:
                     moves.append(Move((r, c), (end_row, end_col), self.board))
 
     def _get_bishop_moves(self, r, c, moves):
-        pass
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        enemy_color = 'b' if self.white_to_move else 'w'
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + d[0] * i
+                end_col = c + d[1] * i
+                if 0 <= end_row < 8 and 0 <= end_col < 8:
+                    end_piece = self.board[end_row][end_col]
+                    if end_piece == '--':
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                    elif end_piece[0] == enemy_color:
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                        break
+                    else:  # friendly piece
+                        break
+                else:  # off board
+                    break
+
+    
 
     def _get_queen_moves(self, r, c, moves):
         pass
